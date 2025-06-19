ffb28b9b222c908b6ad82e40c2218c3764a85542
martinaspeciale <mspeciale127001@gmail.com>
2025-06-19 17:04:04 +0200

    Add Dataset.py as first simple OOP exercise

---
+++ python/Dataset.py
@@ -0,0 +1,56 @@
+import random 
+
+class Dataset:
+    def __init__(self, name, data, labels):
+        self.name=name
+        self.data=data
+        self.labels=labels
+    def summary(self):
+        num_samples = len(self.data)
+        mean_value = sum(self.data)/num_samples if num_samples>0 else 0
+        print(f"Dataset: {self.name}")
+        print(f"Number of samples: {num_samples}")
+        print(f"Mean of data: {mean_value:.2f}")
+    def add_sample(self, sample, label):
+        self.data.append(sample)
+        self.labels.append(label)
+    def normalize(self):
+        if not self.data:
+            print("No data to normalize")
+            return 
+        min_val = min(self.data)
+        max_val = max(self.data)
+
+        if min_val == max_val:
+            # all values are equal 
+            self.data = [0.5 for _ in self.data]
+            return 
+
+        self.data = [(x-min_val)/(max_val-min_val) for x in self.data]
+
+    def split_train_test(self, test_ratio):
+        combined = list(zip(self.data, self.labels))
+        random.shuffle(combined)
+
+        test_size = int(len(combined)*test_ratio)
+
+        test_set = combined[:test_size]
+        train_set = combined[test_size:]
+
+        train_data, train_labels = zip(*train_set) if train_set else ([],[])
+        test_data, test_labels = zip(*test_set) if test_set else ([], [])
+
+        train_dataset = Dataset("f{self.name}_train", list(train_data), list(train_labels))
+        test_dataset = Dataset(f"{self.name}_test", list(test_data), list(test_labels))
+
+        return train_dataset, test_dataset 
+
+
+class ImageDataset(Dataset):
+    def __init__(self, name, data, labels, image_size):
+        super().__init__(self, name, data, labels)
+        self.image_size=image_size
+    
+    def summary(self):
+        super().summary()
+        print(f"Image size: {self.image_size}")
